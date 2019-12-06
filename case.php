84ff69f5608e8522c495e32f63c49d2ceef2d2c3
Roman Shirokov <r.shirokov@studionx.ru>
2019-12-06 18:21:21 +0300

    Add text

--- case.php
+++ case.php
@@ -1,1 +1,34 @@
 <?php 
+
+
+class NotificationService
+{
+    public function notify(User $user, $text)
+    {
+        $emailNotificator = new EmailNotificator();
+        $smsNotificator = new SmsNotificator();
+        $emailNotificator->sendEmail($user->email, $text);
+        $smsNotificator>sendSms($user->phone, $text);
+    }
+}
+
+class EmailNotificator
+{
+    public function sendEmail($email, $text)
+    { ... }
+}
+ 
+class SmsNotificator
+{
+    public function sendSms($phone, $text)
+    { ... }
+}
+
+//Этот сервис сконфигурирован и отдан в клиентский код для выполнения рассылки
+// Инициализация и конфигурация сервиса
+$service = new NotificationService();
+// Клиентский код с доступом к готовому к работе объекту сервиса рассылки
+$text = 'Какой-то текст';
+foreach ($users as $user) {
+    $service->notify($user, $text);
+}
